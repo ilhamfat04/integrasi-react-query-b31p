3bd95a0054fd301710940bb2805a3b01028714fa
= <=>
2022-03-16 11:27:17 +0700

    update

--- client/src/pages/UpdateProductAdmin.js
+++ client/src/pages/UpdateProductAdmin.js
@@ -8,8 +8,10 @@ import CheckBox from "../components/form/CheckBox";
 import dataProduct from "../fakeData/product";
 
 // Import useQuery and useMutation here ...
+import { useQuery, useMutation } from "react-query";
 
-// Get API config here ...
+// Import API config
+import { API } from "../config/api";
 
 export default function UpdateProductAdmin() {
   const title = "Product admin";
@@ -25,15 +27,103 @@ export default function UpdateProductAdmin() {
   const [product, setProduct] = useState({}); //Store product data
 
   // Create variabel for store data with useState here ...
+  const [form, setForm] = useState({
+    image: "",
+    name: "",
+    desc: "",
+    price: "",
+    qty: "",
+  });
 
   // Create process for handle fetching detail product data by id from database with useQuery here ...
+  let { productRefetch } = useQuery("productCache", async () => {
+    const config = {
+      headers: {
+        Authorization: "Basic " + localStorage.token,
+      },
+    };
+    const response = await api.get("/product/" + id, config);
+    setForm({
+      name: response.data.name,
+      desc: response.data.desc,
+      price: response.data.price,
+      qty: response.data.qty,
+      image: response.data.image,
+    });
+    setProduct(response.data);
+  });
+
   // Create process for handle fetching category data from database with useQuery here ...
+  let { categoriesRefetch } = useQuery("categoriesCache", async () => {
+    const response = await api.get("/categories");
+    setCategories(response.data);
+  });
+
 
   // Create function for handle if category selected here ...
+  const handleChangeCategoryId = (e) => {
+    const id = e.target.value;
+    const checked = e.target.checked;
+
+    if (checked == true) {
+      // Save category id if checked
+      setCategoryId([...categoryId, parseInt(id)]);
+    } else {
+      // Delete category id from variable if unchecked
+      let newCategoryId = categoryId.filter((categoryIdItem) => {
+        return categoryIdItem != id;
+      });
+      setCategoryId(newCategoryId);
+    }
+  };
 
   // Create function for handle change data on form variabel here ...
+  const handleChange = (e) => {
+    setForm({
+      ...form,
+      [e.target.name]: e.target.type === "file" ? e.target.files : e.target.value,
+    });
+
+    // Create image url for preview
+    if (e.target.type === "file") {
+      let url = URL.createObjectURL(e.target.files[0]);
+      setPreview(url);
+    }
+  };
 
   // Create function for handle insert new product data with useMutation here ...
+  const handleSubmit = useMutation(async (e) => {
+    try {
+      e.preventDefault();
+
+      // Store data with FormData as object
+      const formData = new FormData();
+      if (preview) {
+        formData.set("image", form.image[0], form.image[0]?.name);
+      }
+      formData.set("name", form.name);
+      formData.set("desc", form.desc);
+      formData.set("price", form.price);
+      formData.set("qty", form.qty);
+      formData.set("categoryId", categoryId);
+
+      // Configuration
+      const config = {
+        method: "PATCH",
+        headers: {
+          Authorization: "Basic " + localStorage.token,
+        },
+        body: formData,
+      };
+
+      // Insert product data
+      const response = await api.patch("/product/" + product.id, config);
+
+      history.push("/product-admin");
+    } catch (error) {
+      console.log(error);
+    }
+  });
 
   useEffect(() => {
     const newCategoryId = product?.categories?.map((item) => {
@@ -42,6 +132,8 @@ export default function UpdateProductAdmin() {
 
     setCategoryId(newCategoryId);
   }, [product]);
+
+  console.log(preview);
 
   return (
     <>
@@ -68,7 +160,7 @@ export default function UpdateProductAdmin() {
               ) : (
                 <div>
                   <img
-                    src={URL.createObjectURL(preview[0])}
+                    src={URL.createObjectURL(form.image[0])}
                     style={{
                       maxWidth: "150px",
                       maxHeight: "150px",
